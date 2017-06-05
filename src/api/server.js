a696e9720c9c2425a9c5e9ca632ccce0662f4010
sepiropht <williamelimbi@gmail.com>
2017-06-05 05:13:35 +0200

    new auth strategy

--- src/api/server.js
+++ src/api/server.js
@@ -1,23 +1,25 @@
 const Express = require('express')
 
-const oauth = require('oauth')
+// const oauth = require('oauth')
 const session = require('express-session')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const TwitterStrategy = require('passport-twitter').Strategy
 
-const {parse} = require('url')
+const passport = require('passport')
+const { parse } = require('url')
 const next = require('next')
 const UserModel = require('./models/user')
 const getRouter = require('./routes')
 const app = new Express()
 
 const dev = process.env.NODE_ENV !== 'production'
-const appNext = next({dev})
+const appNext = next({ dev })
 const handle = appNext.getRequestHandler()
 const setupMongoose = require('./config/setup-mongoose')
-const ImageModel = require('./models/image')
-let secretApp
-let token = ''
+// const ImageModel = require('./models/image')
+
+// let token = ''
 
 module.exports = start
 
@@ -25,15 +27,23 @@ async function start () {
   const cleanupMongoose = await setupMongoose()
   app.use(bodyParser.json())
   app.use(cookieParser())
-  app.use(
-    session({
-      secret: secretApp,
-      cookie: {maxAge: 60000},
-      resave: false,
-      saveUninitialized: false
-    })
+  app.use(session({ secret: 'pinterest' }))
+  app.use(passport.initialize())
+  app.use(passport.session())
+  passport.use(
+    new TwitterStrategy(
+      {
+        consumerKey: 'xLwdUeUNl8TMqRV78tWNLSJYr',
+        consumerSecret: 'UwGLUxOQwe0iHLlOnbT45V8JxNGcMdbCsl8w2SF1KP4yao6Gow',
+        callbackURL: 'http://sepiropht.freeboxos.fr:3000' + '/login-callback'
+      },
+      function (token, tokenSecret, profile, cb) {
+        UserModel.getUserById(profile.id, function (err, user) {
+          return cb(err, { user, token })
+        })
+      }
+    )
   )
-
   app.use(bodyParser.urlencoded())
   appNext.prepare().then(() => {
     app.get('/', (req, res) => {
@@ -47,86 +57,55 @@ async function start () {
     })
   })
 
-  let oauthManager = new oauth.OAuth(
-    'https://api.twitter.com/oauth/request_token',
-    'https://api.twitter.com/oauth/access_token',
-    'xLwdUeUNl8TMqRV78tWNLSJYr',
-    'UwGLUxOQwe0iHLlOnbT45V8JxNGcMdbCsl8w2SF1KP4yao6Gow',
-    '1.0A',
-    'http://sepiropht.freeboxos.fr:3000' + '/login-callback',
-    'HMAC-SHA1'
+  app.get('/login', passport.authenticate('twitter'))
+
+  app.get(
+    '/login-callback',
+    passport.authenticate('twitter', { session: false }),
+    function (req, res) {
+      console.log('YEAH')
+      console.log(req.user)
+      console.log('YEAH')
+      const parsedUrl = parse(req.url, true)
+      const resbis = Object.assign(res, {
+        user: req.user.user.pop(),
+        token: req.user.token
+      })
+      appNext.render(req, resbis, '/', parsedUrl)
+    }
   )
-  app.get('/login', function (req, res) {
-    oauthManager.getOAuthRequestToken(function (err, token, secret, results) {
-      if (err) {
-        res.send('error getting request token: ' + err)
-      } else {
-        req.session.oauth_token = token
-        req.session.oauth_secret = secret
-        secretApp = secret
-        res.redirect(
-          'https://twitter.com/oauth/authenticate?oauth_token=' + token
-        )
-      }
-    })
-  })
-
-  app.post('/updateLike', (req, res) => {
-    ImageModel.updateLike(req.body.imageId, req.body.userId, (err, Images) => {
-      if (err) console.log(err)
-      console.log('before return', Images)
-      res.json(Images)
-    })
-  })
-
-  app.get('/login-callback', function (req, res) {
-    req.session.oauth_verifier = req.query.oauth_verifier
-
-    oauthManager.getOAuthAccessToken(
-      req.session.oauth_token,
-      req.session.oauth_secret,
-      req.session.oauth_verifier,
-      function (err, accessToken, accessSecret, results) {
-        if (err) {
-          res.send('error getting access token: ' + err)
-        } else {
-          token = req.session.oauth_token
-          UserModel.getUserById(results.user_id, function (err, user) {
-            if (err) console.log(err)
-            // console.log("user", user);
-            if (user.length > 0) {
-              // store.dispatch({
-              //   type: LOGGED_IN,
-              //   payload: results
-              // });
-              //  console.log(req.query);
-              const resbis = Object.assign(res, {resbis: user, token: token})
-              const parseUrl = parse(req.url, true)
-              appNext.render(req, resbis, '/', parseUrl)
-            } else {
-              const newUser = new UserModel(results)
-
-              UserModel.create(newUser, (err, user) => {
-                if (err) console.log(err)
-                console.log('user was not in base', results)
-                // store.dispatch({
-                //   type: LOGGED_IN,
-                //   payload: results
-                // });
-                //    console.log(req.query);
-                const parsedUrl = parse(req.url, true)
-                const resbis = Object.assign(res, {
-                  userInfo: results,
-                  token: token
-                })
-                appNext.render(req, resbis, '/', parsedUrl)
-              })
-            }
-          })
-        }
-      }
-    )
-  })
+  // app.get('/login-callback', function (req, res) {
+  //
+  //     function (err, accessToken, accessSecret, results) {
+  //       if (err) {
+  //         res.send('error getting access token: ' + err)
+  //       } else {
+  //         token = req.session.oauth_token
+  //         UserModel.getUserById(results.user_id, function (err, user) {
+  //           if (err) console.log(err)
+  //           if (user.length > 0) {
+  //             const resbis = Object.assign(res, { resbis: user, token: token })
+  //             const parseUrl = parse(req.url, true)
+  //             appNext.render(req, resbis, '/', parseUrl)
+  //           } else {
+  //             const newUser = new UserModel(results)
+  //             UserModel.create(newUser, (err, user) => {
+  //               if (err) console.log(err)
+  //               console.log('user was not in base', results)
+  //
+  //               const parsedUrl = parse(req.url, true)
+  //               const resbis = Object.assign(res, {
+  //                 userInfo: results,
+  //                 token: token
+  //               })
+  //               appNext.render(req, resbis, '/', parsedUrl)
+  //             })
+  //           }
+  //         })
+  //       }
+  //     }
+  //
+  // })
   app.use(getRouter())
   return new Promise(resolve => {
     const server = app.listen(process.env.PORT || 3000, () => {
