8518e1c0d9922a85dfe998e425fcd3733bc7151c
sepiropht <williamelimbi@gmail.com>
2017-06-10 09:18:36 +0200

    remove plugin for pinterest style,only css now

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,9 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Image from './Image'
-import {connect} from 'react-redux'
-import {CSSGrid, measureItems, makeResponsive, layout} from 'react-stonecutter'
-import {updateLike} from '../actions/Images'
-import {compose, lifecycle} from 'recompose'
-const Grid = makeResponsive(measureItems(CSSGrid, {measureImages: true}), {
-  maxWidth: 1920,
-  minPadding: 100
-})
+import { connect } from 'react-redux'
+import { updateLike } from '../actions/Images'
+import { compose, lifecycle } from 'recompose'
+import { stylesheet, classNames } from './home.css'
 
 const mapStateToProps = state => ({
   images: state.Images,
@@ -23,9 +19,11 @@ const withConfig = lifecycle({
 })
 const home = props => {
   console.log(props.images, 'IMAGGGGE')
-  const ImagesCollection = props.images.map((image, index) => (
-    <li key={index}>
+  const ImagesCollection = props.images
+    .filter(image => image.url.length > 0)
+    .map((image, index) => (
       <Image
+        key={index}
         data={image}
         isUserLike={image.likes.some(id => id === props.user.user_id)}
         updateLike={() => {
@@ -38,23 +36,76 @@ const home = props => {
           )
         }}
       />
-    </li>
-  ))
+    ))
   return (
-    <div className="container">
-      <Grid
-        component="ul"
-        columns={4}
-        columnWidth={250}
-        gutterWidth={5}
-        gutterHeight={10}
-        layout={layout.pinterest}
-        duration={800}
-        easing="ease-out"
-        springConfig={{stiffness: 170, damping: 26}}
-      >
+    <div>
+      <div className={classNames.columns}>
+        <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
         {ImagesCollection}
-      </Grid>
+        <figure>
+          <img
+            src="//s3-us-west-2.amazonaws.com/s.cdpn.io/4273/cinderella.jpg"
+          />
+          <figcaption>
+            Cinderella wearing European fashion of the mid-1860’s
+          </figcaption>
+        </figure>
+
+        <figure>
+          <img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/4273/rapunzel.jpg" />
+          <figcaption>Rapunzel, clothed in 1820’s period fashion</figcaption>
+        </figure>
+
+        <figure>
+          <img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/4273/belle.jpg" />
+          <figcaption>Belle, based on 1770’s French court fashion</figcaption>
+        </figure>
+
+        <figure>
+          <img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/4273/mulan_2.jpg" />
+          <figcaption>Mulan, based on the Ming Dynasty period</figcaption>
+        </figure>
+
+        <figure>
+          <img
+            src="//s3-us-west-2.amazonaws.com/s.cdpn.io/4273/sleeping-beauty.jpg"
+          />
+          <figcaption>
+            Sleeping Beauty, based on European fashions in 1485
+          </figcaption>
+        </figure>
+
+        <figure>
+          <img
+            src="//s3-us-west-2.amazonaws.com/s.cdpn.io/4273/pocahontas_2.jpg"
+          />
+          <figcaption>
+            Pocahontas based on 17th century Powhatan costume
+          </figcaption>
+        </figure>
+
+        <figure>
+          <img
+            src="//s3-us-west-2.amazonaws.com/s.cdpn.io/4273/snow-white.jpg"
+          />
+          <figcaption>
+            Snow White, based on 16th century German fashion
+          </figcaption>
+        </figure>
+
+        <figure>
+          <img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/4273/ariel.jpg" />
+          <figcaption>Ariel wearing an evening gown of the 1890’s</figcaption>
+        </figure>
+
+        <figure>
+          <img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/4273/tiana.jpg" />
+          <figcaption>
+            Tiana wearing the <i>robe de style</i> of the 1920’s
+          </figcaption>
+        </figure>
+        <small>Art © <a href="//clairehummel.com">Claire Hummel</a></small>
+      </div>
     </div>
   )
 }
--- components/Image.js
+++ components/Image.js
@@ -1,70 +1,37 @@
 import Router from 'next/router'
+import Head from 'next/head'
+import { stylesheet, classNames } from './image.css'
 const handler = id => Router.replace({
   pathname: '/profil',
-  query: {id: id}
+  query: { id: id }
 })
 const Image = props => {
-  const me = 'willo'
   console.log(props)
   return (
-    <div>
-      <img
-        style={{maxWidth: '300px', maxHeight: '300px'}}
-        src={props.data.url}
-        alt={'nothing'}
-      />
-      <h3 className="title">{props.data.title}</h3>
-      <div className="footer">
-        <div onClick={handler.bind(undefined, props.data.userId)}>
-          <a>
-            <img className="profile-picture" src="images.png" alt="nothing" />
-          </a>
+    <figure>
+      <figcaption>
+        <img src={props.data.url} alt={'nothing'} />
+        <h3 className={classNames.title}>{props.data.title}</h3>
+        <div className={classNames.footer}>
+          <div onClick={handler.bind(undefined, props.data.userId)}>
+            <a>
+              <img
+                className={classNames.profilePicture}
+                src="images.png"
+                alt="nothing"
+              />
+            </a>
+          </div>
+          <div
+            className={classNames.heart}
+            onClick={props.updateLike}
+            style={!props.isUserLike ? { display: 'none!important' } : {}}
+          />
+          <span>{props.data.likes.length}</span>
         </div>
-        <div
-          className="heart"
-          onClick={props.updateLike}
-          style={!props.isUserLike ? {display: 'none!important'} : {}}
-        />
-        <span>{props.data.likes.length}</span>
-      </div>
-      <style jsx>
-        {
-          `
-        .heart {
-            margin: auto;
-            background-color: pink;
-            height: 15px;
-            width: 15px;
-            transform: rotate(-45deg);
-          }
-          .heart:after {
-            background-color: pink;
-            content: "";
-            border-radius: 50%;
-            position: absolute;
-            width: 15px;
-            height: 15px;
-            top: 0px;
-            left: 7.5px;
-          }
-          .heart:before {
-            content: '';
-            background-color: pink;
-            border-radius: 50%;
-            position: absolute;
-            width: 15px;
-            height: 15px;
-            top: -7.5px;
-            left: 0px;
-          }
-          .heart:hover {
-             transform: scale(2.25);
-
-          }
-  `
-        }
-      </style>
-    </div>
+      </figcaption>
+      <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+    </figure>
   )
 }
 
